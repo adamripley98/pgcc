286e0592a6d4dca288ebffe2e35cd3aec16a36b8
Adam Ripley <aripley@wharton.upenn.edu>
2020-08-10 10:19:40 -0800

    contact page

---
+++ src/components/ContactForm.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import Card from './Card';
+import Button from './Button';
+
+const ContactForm = () => (
+  <div className="container mx-auto">
+    <div className="flex justify-center items-center flex-col">
+      <h1 className="text-2xl mb-8">
+        Inquire about our club through this form and we will respond within 48 hours.
+      </h1>
+
+      <Card className="mx-4 lg:w-2/3">
+        <div className="lg:flex">
+          <input
+            type="text"
+            className="focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-4 px-4 mr-2 my-2 w-full lg:w-1/2"
+            placeholder="Name"
+            name="name"
+          />
+          <input
+            type="email"
+            className="focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-4 px-4 my-2 w-full lg:w-1/2"
+            placeholder="Email"
+            name="email"
+          />
+        </div>
+        <textarea
+          rows="4"
+          className="focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-4 px-4 mr-2 my-2 w-full"
+          placeholder="Write your message..."
+          name="message"
+        />
+      </Card>
+      <Button size="md" className="mt-10">
+        Send Message
+      </Button>
+    </div>
+  </div>
+);
+
+export default ContactForm;
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,14 +1,9 @@
 import React from 'react';
 import Layout from '../components/layout/Layout';
 import SectionSpacer from '../components/layout/SectionSpacer';
-import PastEventCard from '../components/PastEventCard';
-import UpcomingEventCard from '../components/UpcomingEventCard';
-import LabelText from '../components/LabelText';
 import MailList from '../components/MailList';
-import pastEvents from '../data/past-events';
-import upcomingEvents from '../data/upcoming-events';
-import FacebookEvents from '../components/FacebookEvents';
 import Banner from '../components/Banner';
+import ContactForm from '../components/ContactForm';
 import FadeInSection from '../components/layout/FadeInSection';
 
 export default () => (
@@ -19,7 +14,7 @@ export default () => (
     />
     <FadeInSection>
       <section className="pt-20">
-        <div className="container mx-auto px-8">Contact us</div>
+        <ContactForm />
       </section>
     </FadeInSection>
     <FadeInSection>
