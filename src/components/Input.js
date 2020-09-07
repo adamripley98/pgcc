a75977407773469a8814fcde2215f6cb1e7cc403
Adam Ripley <aripley@wharton.upenn.edu>
2020-09-07 17:28:27 -0600

    mailchimp integration done

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Button from './Button';
 
-const Input = ({ inputText, buttonText, className = '', handleSubmit }) => (
+const Input = ({ inputText, buttonText, className = '', handleSubmit, handleChange }) => (
   <>
     <input
       type="email"
       className="focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg sm:w-2/3 md:w-1/2 lg:w-1/3 py-4 px-4 mr-2"
       placeholder={inputText}
       name="email"
+      onChange={handleChange}
     />
     <Button size="lg" handleSubmit={handleSubmit}>
       {buttonText}
--- src/components/MailList.js
+++ src/components/MailList.js
@@ -9,34 +9,52 @@ class MailList extends React.Component {
       email: '',
       result: ''
     };
+    this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
   async handleSubmit(e) {
-    console.log('enters submit');
-    e.preventDefault();
-    const result = await addToMailchimp('aripley@wharton.upenn.edu');
-    // I recommend setting `result` to React state
-    // but you can do whatever you want
-    this.setState({ result: result.msg });
-    console.log('what is result', result);
+    if (this.state.email) {
+      e.preventDefault();
+      const result = await addToMailchimp(this.state.email);
+      if (result.result === 'success') {
+        this.setState({ result: 'Thank you for subscribing!' });
+      } else {
+        this.setState({
+          result: 'Error subscribing. Email us at pgcc.comm@gmail.com to get on the maillist.'
+        });
+      }
+    }
   }
 
   render() {
     return (
       <section className="lg:mx-12 mx-auto py-16 bg-gray-200 rounded-lg text-center">
         <h3 className="text-3xl font-bold leading-tight">Subscribe to our Mailing List</h3>
-        <p className="mt-8 mx-8 text-xl font-light">
-          Sign up for our mailing list to become a PGCC member, get updates about upcoming events,
-          and gain valuable resources.
-        </p>
-        <p className="mt-8">
-          <Input
-            inputText="benfranklin@upenn.edu"
-            handleSubmit={this.handleSubmit}
-            buttonText="Submit"
-          />
-        </p>
+        {this.state.result ? (
+          <p className="mt-8 mx-8 text-xl font-light">{this.state.result}</p>
+        ) : (
+          <>
+            <p className="mt-8 mx-8 text-xl font-light">
+              Sign up for our mailing list to become a PGCC member, get updates about upcoming
+              events, and gain valuable resources.
+            </p>
+            <p className="mt-8">
+              <Input
+                inputText="benfranklin@upenn.edu"
+                handleSubmit={this.handleSubmit}
+                handleChange={this.handleChange}
+                buttonText="Submit"
+              />
+            </p>
+          </>
+        )}
       </section>
     );
   }
