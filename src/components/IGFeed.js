ae4301b451ee5a9e93dc7c9f0b3a0f1ad4b5a7b4
Adam Ripley <aripley@wharton.upenn.edu>
2020-10-01 12:11:02 -0700

    ig feed

--- src/components/IGFeed.js
+++ src/components/IGFeed.js
@@ -1,35 +1,42 @@
 import React from 'react';
-import request from 'request';
-import cheerio from 'cheerio';
 
-class IGFeed extends React.Component {
-  componentDidMount() {
-    const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
-    const targetUrl =
-      'https://embedsocial.com/facebook_album/instagram_album_media/9fffdfec5d37bf8f66d4ece32184079a51042de3';
-    request(proxyUrl + targetUrl, (error, response, body) => {
-      const $ = cheerio.load(body);
-      $('.estitle').addClass('hidden');
-      $('.espagination').addClass('hidden');
-      $('.powered-by').addClass('hidden');
-      $('.developed-by').addClass('hidden');
-      document.getElementById('gallery-html').innerHTML = $.html();
-    });
+let width = 800;
+if (typeof window !== `undefined`) {
+  width = window.innerWidth > 0 ? window.innerWidth : screen.width;
+}
+
+const getWidth = () => {
+  if (width > 950) {
+    return '900px';
   }
+  if (width > 700) {
+    return '600px';
+  }
+  return '400px';
+};
 
-  render() {
-    return (
-      <div className="container">
+const IGFeed = () => {
+  return (
+    <div className="container mx-auto text-center flex justify-center">
+      <div>
         <h5 className="text-3xl font-bold leading-tight text-center mb-12">
           Find us on{' '}
           <a href="https://www.instagram.com/upenn_pgcc/" rel="noreferrer" target="_blank">
             Instagram
           </a>
         </h5>
-        <div id="gallery-html" />
+        <iframe
+          title="ig-feed"
+          src="https://embedsocial.com/facebook_album/pro_instagram/9fffdfec5d37bf8f66d4ece32184079a51042de3"
+          width={getWidth()}
+          height="1300px"
+          frameBorder="0"
+          marginHeight="0"
+          marginWidth="0"
+        />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default IGFeed;
