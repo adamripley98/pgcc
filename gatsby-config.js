60f580d393a01233f69e71b985a4f12c42e3ba77
Adam Ripley <aripley@wharton.upenn.edu>
2020-10-19 15:15:51 -0600

    adding analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,6 +19,12 @@ module.exports = {
           'https://penngraduateconsultinggroup.us7.list-manage.com/subscribe/post?u=42a16ebbf91719c54d9692097&amp;id=80f39fd46d', // string; add your MC list endpoint here; see instructions below
         timeout: 5000 // number; the amount of time, in milliseconds, that you want to allow mailchimp to respond to your request before timing out. defaults to 3500
       }
+    },
+    {
+      resolve: `gatsby-plugin-gtag`,
+      options: {
+        trackingId: `UA-172345380-2`
+      }
     }
   ]
 };
