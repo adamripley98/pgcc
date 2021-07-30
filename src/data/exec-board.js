82d1fd4d84bbffd898135d403b69d1229c749530
pgccboard <pgcc.comm@gmail.com>
2021-07-29 22:43:46 -0400

    Updated eboard

--- src/data/exec-board.js
+++ src/data/exec-board.js
@@ -94,14 +94,13 @@ export default [
   },
   {
     content:
-      'Mohit is a graduate student from the Mechanical Engineering and Applied Mechanics. He is very passionate about management consulting and wants to pursue it as a career. During his time at Penn, he managed multiple consulting projects for high-profile clients across the world and worked as a Research Analyst for Mack Institute at the Wharton School. As a VP of Finance, he is motivated to extend strategic collaboration with corporate clients and identify funding opportunities to expand the reach of PGCC. In his free time, Mohit enjoys stock trading, traveling, and spending quality time with friends.',
-    name: 'Mohitkumar Patel',
+      'Abhinav is pursuing a Master\'s in Data Science. A published TEDx speaker, his primary area of focus is algorithmic bias and its various applications. Abhinav has worked with data extensively in the finance/technology sectors and hopes to apply solutions for bias in these industries. As VP of Finance, Abhinav aims to expand PGCC\'s reach through strategic corporate sponsorships with both general and specialty consulting firms. In his free time, he plays tennis, writes short stories, and sings.',
+    name: 'Abhinav Raghunathan',
     title: 'VP of Finance',
-    linkedin: 'https://www.linkedin.com/in/mohitpatel11/',
-    email: 'mohit11@seas.upenn.edu',
-    image: Mohit
+    linkedin: 'https://linkedin.com/in/abhinavraghunathan',
+    email: 'abhirags@seas.upenn.edu',
+    image: Abhinav
   },
-
   {
     content:
       'Angela is pursuing her master degree in Social Policy and Data Analytics. She has been involved in various strategic planning and data mining research activities and conferences in both tech- and management consulting companies. As VP for Guided Case Practice, Angela aims to organize consulting workshops and case-practice sessions for Penn graduate students to enhance their consulting skill set and prepare for future career development.',
@@ -110,5 +109,14 @@ export default [
     linkedin: 'https://www.linkedin.com/in/jingyi-angela-guo-92995b205/',
     email: 'angelagj@upenn.edu',
     image: Angela
+  }
+  {
+    content:
+      'Christina Zheng is a second-year Master of Social Policy & Data Analytics (MSSP+DA) student at School of Social Policy & Practice. Prior to pursuing her master’s degree, she graduated from University of Toronto with a BCom in Management and Marketing. Her career interests are focused on public sector consulting and strategy, where she plans to leverage her experience in policy work, data insights, and sales & marketing to deliver better social change. As the VP for Social & Student Engagement, Christina hopes to build connections between PGCC and graduate students who are passionate about a career in consulting. When she is not busy, she enjoys playing the piano and creating original music.',
+    name: 'Christina Zheng',
+    title: 'VP for Social & Student Engagement',
+    linkedin: 'https://www.linkedin.com/in/christina-zheng/',
+    email: 'leyin@upenn.edu',
+    image: Christina
   }
 ];
