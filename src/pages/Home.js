4bbe2b87a1afe7885c9be65f3e10a522bf1dd419
tfgu98 <tfgu@sas.upenn.edu>
2022-05-18 23:05:02 -0400

    commit test 2

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -23,7 +23,7 @@ export default () => (
         <div className="container mx-auto px-8 lg:flex lg:items-center">
           <div className="text-center lg:text-left lg:w-1/2">
             <h1 className="text-4xl lg:text-5xl xl:text-6xl font-bold leading-none">
-              PENN Graduate Consulting Club
+              UPENN Graduate Consulting Club
             </h1>
             <p className="text-xl lg:text-2xl mt-6 font-light">
               Premiere student-run consulting club at the University of Pennsylvania, 500+ members
