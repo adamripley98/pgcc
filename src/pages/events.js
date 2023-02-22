78eb6ebabf179858cb90fe764409b3d27a7acae9
derek <72407929+pgccboard@users.noreply.github.com>
2023-02-22 11:56:39 -0500

    imported flyer as pdf and called it in code

--- src/pages/events.js
+++ src/pages/events.js
@@ -6,7 +6,7 @@ import MailList from '../components/MailList';
 import FacebookEvents from '../components/FacebookEvents';
 import Banner from '../components/Banner';
 import FadeInSection from '../components/layout/FadeInSection';
-import Flyer from '../images/resources/flyer2.png';
+import Flyer from '../images/resources/PYPC_flyer.pdf';
 
 export default () => (
   <Layout>
